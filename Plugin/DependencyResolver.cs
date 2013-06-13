cc0c9cec88de4ef8889ce9bb29397b8f450c0d53
Maarten Abbink <m.abbink@fugro.nl>
2013-06-13 15:14:46 +0200

    added reactive DFS mock dependency traversal

---
+++ Plugin/DependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace Plugin
+{
+    public class DependencyResolver
+    {
+        static string greetings = "PONG!";
+
+        public static void GetAllDependencies()
+        {
+            AppDomain otherDomain = AppDomain.CreateDomain("otherDomain");
+            
+
+            greetings = "PING!";
+            MyCallBack();
+            //this does not quite work with NUnit
+            otherDomain.DoCallBack(new CrossAppDomainDelegate(MyCallBack));
+            AppDomain.Unload(otherDomain);
+        }
+
+        static public void MyCallBack()
+        {
+            string name = AppDomain.CurrentDomain.FriendlyName;
+
+            if (name == AppDomain.CurrentDomain.SetupInformation.ApplicationName)
+            {
+                name = "defaultDomain";
+            }
+            Console.WriteLine(greetings + " from " + name);
+        }
+    }
+}
